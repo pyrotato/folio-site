a0e09ad2a7ebae8bb019c1dce817b9348df56da7
Nicole Wong <88985409+pyrotato@users.noreply.github.com>
2023-08-18 23:58:18 +0000

    yosh

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -63,7 +63,12 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideIn" }}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "slideIn",
+            }}
             alt={imageAltText}
           />
         </div>
